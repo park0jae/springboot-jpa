e05333b507a2dbc604edfdfff1671ac4cb65a6e3
park0jae <okvv26@naver.com>
2023-11-17 09:56:10 +0900

    init : 프로젝트 초기 세팅

---
+++ src/main/java/com/zerozae/weekly/WeeklyApplication.java
@@ -0,0 +1,13 @@
+package com.zerozae.weekly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeeklyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeeklyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zerozae/weekly/WeeklyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zerozae.weekly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeeklyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
